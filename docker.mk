55c4f3730c10dd293c449f3a2ee09ca613f3462e
Alex Egorov <alex202@egorov.net>
2022-06-04 01:39:39 +0300

    check vars in Makefile tasks

--- docker.mk
+++ docker.mk
@@ -1,21 +1,31 @@
-include build.properties
+check_defined = \
+    $(strip $(foreach 1,$1, \
+        $(call __check_defined,$1,$(strip $(value 2)))))
+__check_defined = \
+    $(if $(value $1),, \
+      $(error Undefined $1$(if $2, ($2))))
 
-check:
-	ifndef DOCKER_REGISTRY
-	$(error "DOCKER_REGISTRY is not defined")
-	endif
-
-build: check
+build:
+	$(call check_defined, DOCKER_REGISTRY)
+	$(call check_defined, DOCKER_NAME)
 	docker build -t $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) --build-arg VERSION=$(VERSION) .
 
-build-all: check
+build-all:
+	$(call check_defined, DOCKER_REGISTRY)
+	$(call check_defined, DOCKER_NAME)
+	$(call check_defined, VERSIONS)
 	$(foreach version, $(VERSIONS), make build VERSION=$(version);)
 
-release: check
+release:
+	$(call check_defined, DOCKER_REGISTRY)
+	$(call check_defined, DOCKER_NAME)
 	docker manifest inspect $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) || ( \
 		make build VERSION=$(VERSION) && \
 		docker push $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) \
 	)
 
-release-all: check
+release-all:
+	$(call check_defined, DOCKER_REGISTRY)
+	$(call check_defined, DOCKER_NAME)
+	$(call check_defined, VERSIONS)
 	$(foreach version, $(VERSIONS), make release VERSION=$(version);)
