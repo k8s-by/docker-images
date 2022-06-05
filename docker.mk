dd7d75d042d9d5247d34023f7ad280dd5f65508f
Alex Egorov <alex202@egorov.net>
2022-06-05 09:12:49 +0300

    kaniko

--- docker.mk
+++ docker.mk
@@ -5,10 +5,27 @@ __check_defined = \
     $(if $(value $1),, \
       $(error Undefined $1$(if $2, ($2))))
 
+DOCKERFILE?=Dockerfile
+KANIKO_CONTEXT?=dir:///workspace/
+KANIKO_LOGLEVEL?=info
+KANIKO_CACHE?="false"
+KANIKO_CACHE_REPO?=
+
+
 build:
 	$(call check_defined, DOCKER_REGISTRY)
 	$(call check_defined, DOCKER_NAME)
-	docker build -t $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) --build-arg VERSION=$(VERSION) .
+	$(call check_defined, VERSIONS)
+	@docker run -v $$(pwd):/workspace \
+           -v ~/.docker/config.json:/kaniko/.docker/config.json:ro \
+           gcr.io/kaniko-project/executor:latest \
+             --cleanup \
+             --verbosity=$(KANIKO_LOGLEVEL) \
+             --dockerfile=$(DOCKERFILE) \
+             --context=$(KANIKO_CONTEXT) \
+             --cache=$(KANIKO_CACHE) $(KANIKO_CACHE_REPO) \
+             --destination=$(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) \
+             --build-arg=VERSION=$(VERSION)
 
 build-all:
 	$(call check_defined, DOCKER_REGISTRY)
@@ -21,7 +38,7 @@ release:
 	$(call check_defined, DOCKER_NAME)
 	docker manifest inspect $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) || ( \
 		make build VERSION=$(VERSION) && \
-		docker push $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) \
+		#docker push $(DOCKER_REGISTRY)/$(DOCKER_NAME):$(VERSION) \
 	)
 
 release-all:
